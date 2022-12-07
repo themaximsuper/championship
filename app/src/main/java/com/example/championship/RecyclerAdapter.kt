a755e6efff2e891aaabfbf1bc70f38df195230aa
malin <malinkovichmax@gmail.com>
2022-12-07 20:38:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/championship/RecyclerAdapter.kt
@@ -0,0 +1,10 @@
+package com.example.championship
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewHolder(item: View) : RecyclerView.ViewHolder(item)
+
+class RecyclerAdapter(val list : List<>) : RecyclerView.Adapter<>() {
+
+}
